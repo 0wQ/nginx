b31e35609a52d9c848835c9650a1c6390dd08f85
Mizore <xvz@live.com>
2021-07-11 22:48:00 +0800

    Update ngx_http_autoindex_module.c

--- src/http/modules/ngx_http_autoindex_module.c
+++ src/http/modules/ngx_http_autoindex_module.c
@@ -50,9 +50,9 @@ typedef struct {
 #define NGX_HTTP_AUTOINDEX_JSONP        2
 #define NGX_HTTP_AUTOINDEX_XML          3
 
-#define NGX_HTTP_AUTOINDEX_PREALLOCATE  50
+#define NGX_HTTP_AUTOINDEX_PREALLOCATE  100
 
-#define NGX_HTTP_AUTOINDEX_NAME_LEN     50
+#define NGX_HTTP_AUTOINDEX_NAME_LEN     100
 
 
 static ngx_buf_t *ngx_http_autoindex_html(ngx_http_request_t *r,
